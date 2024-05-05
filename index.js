754f50e323c39acb9708ec64d8b3310015e0cc36
Alex Manuel Gomez Salazar <168044669+Lic-AlexGomez@users.noreply.github.com>
2024-05-05 12:18:45 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,75 +1,27 @@
 
-
-document.getElementById('btnSearch').addEventListener('click', searchMovies);
-
-let resuts = document.getElementById('results');
-
-const urlBase = 'https://api.themoviedb.org/3/search/movie'
-const api_key = 'b7af9f521de40221474e50f530a2d424'
-function searchMovies(){
-    resuts.innerHTML="";
-    let searchInput = document.getElementById('inputSearch').value
-    fetch(`${urlBase}?api_key=${api_key}&query=${searchInput}`)
-    .then(response => response.json())
-    .then(response => displayMovies(response.results))
-}function displayMovies(movies){
- 
-
-    if(movies.length === 0){
-        resultCoresutsntainer.innerHTML= '<p>No se encontraron resultados para tu búsqueda </p>'
-        return
-    }
-
-    movies.forEach(movie => {
-        
-        if(movie.poster_path){
-
-        let urlImg='https://image.tmdb.org/t/p/w400';
-        let posterPath = urlImg + movie.poster_path;
-
-        let card1 = document.createElement('div');
-        card1.classList.add('card');
-        card1.classList.add('rgb');
-        
-        let card2 = document.createElement('div');
-        card2.classList.add('card-image');
-        card2.classList.add('card2');
-        card2.style.backgroundImage=`url('${posterPath}')`;
-   
-        let card3 = document.createElement('div');
-        card3.classList.add('card-text');
-        card3.classList.add('card2');
-
-        let span1 = document.createElement('span');
-        span1.classList.add('date');
-        span1.innerHTML = `Date release: `+movie.release_date;
-
-        let h21 = document.createElement('h2');
-        h21.innerHTML = movie.title;
-        let p1 = document.createElement('p');
-        
-      
-       if(movie.overview.length >335) {
-
-         p1.innerHTML = `${movie.overview.substring(0, 370)}...Read more.`;
-         p1.addEventListener('click', () => {
-             alert(movie.overview) ;
-         })
-       }else{
+let container=document.getElementById("principal");
 
 
-           p1.innerHTML = movie.overview;
-       }
-        
-        resuts.appendChild(card1);
-        card1.appendChild(card2);
-        card1.appendChild(card3);
-        card3.appendChild(h21);
-        card3.appendChild(span1);
-        card3.appendChild(p1);
-    }
-       
-        
-     
-    })
-}
+let contenedorBusqueda = document.createElement("div");
+contenedorBusqueda.setAttribute("class", "container");
+container.appendChild(contenedorBusqueda);
+
+let h1 = document.createElement("h1");
+h1.textContent = "Pelis Search API";
+contenedorBusqueda.appendChild(h1);
+
+let input = document.createElement("input");
+input.setAttribute("type", "text");
+input.setAttribute("id", "inputSearch");
+contenedorBusqueda.appendChild(input);
+
+let button = document.createElement("button");
+button.setAttribute("id", "btnSearch");
+button.setAttribute("class", "rgb");
+button.textContent = "Search";
+contenedorBusqueda.appendChild(button);
+
+let resultContainer= document.createElement("div");
+resultContainer.setAttribute("id", "results");
+container.appendChild(resultContainer);
+
--- script.js
+++ script.js
@@ -1,116 +1,75 @@
-let baseUrl = "https://api.themoviedb.org/3/configuration"
 
-let mainDiv = document.getElementsByTagName("body")[0];
 
-// let container = document.createElement("div");
-// container.setAttribute("class", "container");
+document.getElementById('btnSearch').addEventListener('click', searchMovies);
 
-// let h1 = document.createElement("h1");
-// h1.textContent = "Pelis Search API";
-// let input = document.createElement("input");
-// input.setAttribute("type", "text");
-// input.setAttribute("id", "inputSearch");
+let resuts = document.getElementById('results');
 
-// let button = document.createElement("button");
-// button.setAttribute("id", "btnSearch");
-// button.textContent = "Search";
-// button.addEventListener("click",function(){
+const urlBase = 'https://api.themoviedb.org/3/search/movie'
+const api_key = 'b7af9f521de40221474e50f530a2d424'
+function searchMovies(){
+    resuts.innerHTML="";
+    let searchInput = document.getElementById('inputSearch').value
+    fetch(`${urlBase}?api_key=${api_key}&query=${searchInput}`)
+    .then(response => response.json())
+    .then(response => displayMovies(response.results))
+}function displayMovies(movies){
  
-//     alert("Please enter"); 
 
-// });
-
-
-// mainDiv.appendChild(container);
-// container.appendChild(h1);
-// container.appendChild(input);
-// input.setAttribute("type", "text");
-// container.appendChild(button);
-
-// fetch(baseUrl)
-// .then(data => data.json())
-
-// .then(data => {
-//     console.log(data);
-
-//     let rick= document.createElement("a");
-//     rick.setAttribute("href", data.info.next);
-//     rick.textContent = data.info.next;
-
-//     container.appendChild(rick);
-
-
-// });
-
-// const options = {
-//   method: 'GET',
-//   headers: {
-//     accept: 'application/json',
-//     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiN2FmOWY1MjFkZTQwMjIxNDc0ZTUwZjUzMGEyZDQyNCIsInN1YiI6IjY2MzU5NThlOTU5MGUzMDEyM2JiYzhmZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.3KJhtrevVYpaknKscqkj4aIIJaLqAsyp_3EWGGRtqRQ'
-//   }
-// };
-
-// fetch('https://api.themoviedb.org/3/discover/movie?include_adult=true&include_video=true&language=es-ES&page=1&sort_by=popularity.desc', options)
-//   .then(response => response.json())
-// //   .then(response => console.log(response))
-//   .catch(err => console.error(err));
-
-//   const option = {
-//     method: 'GET',
-//     headers: {
-//       accept: 'application/json',
-//       Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiN2FmOWY1MjFkZTQwMjIxNDc0ZTUwZjUzMGEyZDQyNCIsInN1YiI6IjY2MzU5NThlOTU5MGUzMDEyM2JiYzhmZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.3KJhtrevVYpaknKscqkj4aIIJaLqAsyp_3EWGGRtqRQ'
-//     }
-//   };
-  
-//   fetch('https://api.themoviedb.org/3/movie/823464/videos?language=en-US', option)
-//     .then(response => response.json())
-//     .then(response =>{
-//         console.log(response);
-        
-//         let rick= document.createElement("img");
-//         //  - rick.setAttribute("src", "https://api.themoviedb.org/3/movie/823464/images");
-//           rick.src = `https://image.tmdb.org/t/p/w500${response.backdrops[0].file_path}`;
-//           rick.setAttribute("width", "100%");
-//           mainDiv.appendChild(rick);
-//     })
-//     .catch(err => console.error(err));
-
-//     const optio= {
-//         method: 'GET',
-//         headers: {
-//           accept: 'application/json',
-//           Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiN2FmOWY1MjFkZTQwMjIxNDc0ZTUwZjUzMGEyZDQyNCIsInN1YiI6IjY2MzU5NThlOTU5MGUzMDEyM2JiYzhmZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.3KJhtrevVYpaknKscqkj4aIIJaLqAsyp_3EWGGRtqRQ'
-//         }
-//       };
-      
-//       fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', optio)
-//         .then(response => response.json())
-//         .then(response => console.log(response))
-//         .catch(err => console.error(err));
-
-//         const o= {
-//             method: 'GET',
-//             headers: {
-//               accept: 'application/json',
-//               Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiN2FmOWY1MjFkZTQwMjIxNDc0ZTUwZjUzMGEyZDQyNCIsInN1YiI6IjY2MzU5NThlOTU5MGUzMDEyM2JiYzhmZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.3KJhtrevVYpaknKscqkj4aIIJaLqAsyp_3EWGGRtqRQ'
-//             }
-//           };
-const o= {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiN2FmOWY1MjFkZTQwMjIxNDc0ZTUwZjUzMGEyZDQyNCIsInN1YiI6IjY2MzU5NThlOTU5MGUzMDEyM2JiYzhmZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.3KJhtrevVYpaknKscqkj4aIIJaLqAsyp_3EWGGRtqRQ'
+    if(movies.length === 0){
+        resultCoresutsntainer.innerHTML= '<p>No se encontraron resultados para tu búsqueda </p>'
+        return
     }
-  };
-          fetch('https://api.themoviedb.org/3/movie/823461/watch/providers', o)
-            .then(response => response.json())
-            .then(response => {
-                console.log(response.results.AD);
-                console.log(response.results.AD)
-                let rick= document.createElement("video");
-         
-                   rick.src = `response.results.AD`;
-                  rick.setAttribute("width", "100%");
-                  mainDiv.appendChild(rick);
-            })
+
+    movies.forEach(movie => {
+        
+        if(movie.poster_path){
+
+        let urlImg='https://image.tmdb.org/t/p/w400';
+        let posterPath = urlImg + movie.poster_path;
+
+        let card1 = document.createElement('div');
+        card1.classList.add('card');
+        card1.classList.add('rgb');
+        
+        let card2 = document.createElement('div');
+        card2.classList.add('card-image');
+        card2.classList.add('card2');
+        card2.style.backgroundImage=`url('${posterPath}')`;
+   
+        let card3 = document.createElement('div');
+        card3.classList.add('card-text');
+        card3.classList.add('card2');
+
+        let span1 = document.createElement('span');
+        span1.classList.add('date');
+        span1.innerHTML = `Date release: `+movie.release_date;
+
+        let h21 = document.createElement('h2');
+        h21.innerHTML = movie.title;
+        let p1 = document.createElement('p');
+        
+      
+       if(movie.overview.length >335) {
+
+         p1.innerHTML = `${movie.overview.substring(0, 370)}...Read more.`;
+         p1.addEventListener('click', () => {
+             alert(movie.overview) ;
+         })
+       }else{
+
+
+           p1.innerHTML = movie.overview;
+       }
+        
+        resuts.appendChild(card1);
+        card1.appendChild(card2);
+        card1.appendChild(card3);
+        card3.appendChild(h21);
+        card3.appendChild(span1);
+        card3.appendChild(p1);
+    }
+       
+        
+     
+    })
+}
